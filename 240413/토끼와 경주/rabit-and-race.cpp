0384c8ce59c63aca0f623f513f9302a2c0cce69c
Guho-Kim <64512313+Guho-Kim@users.noreply.github.com>
2024-04-13 02:09:31 +0900

    Title: 토끼와 경주, Time: 2508ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240413/토끼와 경주/rabit-and-race.cpp
+++ 240413/토끼와 경주/rabit-and-race.cpp
@@ -55,6 +55,21 @@ struct compare2{
             }
     }
 };
+bool compare_vec(const Rabbit a, const Rabbit b){
+    if(a.x+a.y == b.x+b.y){
+        if(a.x == b.x){
+            if(a.y == b.y){
+                return a.pid > b.pid;
+            }else{
+                return a.y > b.y;
+            }
+        }else{
+            return a.x > b.x;
+        }
+    }else{
+        return a.x+a.y > b.x+b.y;
+    }
+}
 priority_queue<Rabbit, vector<Rabbit>, compare> pq;
 unordered_map<int, int> dist_info;
 unordered_map<int, int> score_info;
@@ -83,7 +98,7 @@ void play(){
         int jump_cnt=rabbit.jump_cnt+1;
         int x= rabbit.x;
         int y= rabbit.y;
-        priority_queue<Rabbit, vector<Rabbit>, compare2> pq2;
+        vector<Rabbit> nextRabbit;
         for(int i=0; i<4; i++){
             int nx=x;
             int ny=y;
@@ -101,20 +116,23 @@ void play(){
                     ny+=2*dy_;
                 }
             }
-            pq2.push({jump_cnt, nx, ny, pid});
+            nextRabbit.push_back({jump_cnt, nx, ny, pid});
         }
-        Rabbit nRabbit=pq2.top();
+        sort(nextRabbit.begin(), nextRabbit.end(), compare_vec); 
+
+        Rabbit nRabbit=nextRabbit[0];
         hist[nRabbit.pid]=nRabbit;
         pq.push(nRabbit);
 
         total_score+=nRabbit.x+nRabbit.y;
         score_info[rabbit.pid]-=nRabbit.x+nRabbit.y;
     }
-    priority_queue<Rabbit, vector<Rabbit>, compare2> pq3;
+    vector<Rabbit> bestRabbit;
     for(auto r :hist){
-        pq3.push(r.second);
+        bestRabbit.push_back(r.second);
     }
-    int best_pid = pq3.top().pid;
+    sort(bestRabbit.begin(), bestRabbit.end(), compare_vec);
+    int best_pid = bestRabbit[0].pid;
     score_info[best_pid]+=S;
     
 }
